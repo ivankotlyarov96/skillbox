57191b21052efe21157d31a1a0e6f673885e2882
ivankotlyarov96 <118462721+ivankotlyarov96@users.noreply.github.com>
2022-11-23 20:05:05 +0700

    'work1'

---
+++ index.py
@@ -0,0 +1,26 @@
+def check_age(years):
+  while 1:
+                        years = int(input('Введите возраст: '))
+                        if years > 0:
+                            break
+                        print('Возраст должен быть положительным')
+  return years
+
+def check_ogrnip(number):
+  while 1:
+                        number = int(input('Введите ОГРНИП: '))
+                        if len (str(number)) == 15:
+                            break
+                        print('ОГРНИП должен содержать 15 цифр!')
+  return number
+
+def check_payment_account(number):
+  while 1:
+                        number = int(input('Введите расчетный счет: '))
+                        if len (str(number)) == 20:
+                            break
+                        print('Расчётный счёт  должен содержать 20 цифр!')
+  return number
+SEPARATOR = '------------------------------------------'
+
+name = ''fsddfsfds
